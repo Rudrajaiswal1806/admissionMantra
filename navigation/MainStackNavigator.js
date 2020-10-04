2f6d9137753b762ac7ee1c93b71d107b301d4862
Dhruv Rohatgi <dhruvrohatgi53@gmail.com>
2020-10-04 12:19:39 +0530

    Test 2

---
+++ navigation/MainStackNavigator.js
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Councelling from '../screen/Councelling';
+import Course from '../screen/Course';
+import Login from '../screen/Login';
+import Main from '../screen/Main';
+import Pdf from '../screen/Pdf';
+import CourseComp from '../screen/CourseComp';
+import Paper from '../screen/Paper';
+import Register from '../screen/Register';
+const Stack = createStackNavigator()
+
+const MainStackNavigator = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Main' screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name='Course' component={Course} options={{ title: 'Dashboard' }} />
+                <Stack.Screen name='Main' component={Main} options={{ title: 'Bed Bureau' }} />
+                <Stack.Screen name='Login' component={Login} options={{ title: 'Bed Bureau' }} />
+                <Stack.Screen name='Councelling' component={Councelling} options={{ title: 'Bed Bureau' }} />
+                <Stack.Screen name='CourseComp' component={CourseComp} options={{ title: 'Bed Bureau' }} />
+                <Stack.Screen name='Paper' component={Paper} options={{ title: 'Bed Bureau' }} />
+                <Stack.Screen name='Pdf' component={Pdf} options={{ title: 'Bed Bureau' }} />
+                <Stack.Screen name='Register' component={Register} options={{ title: 'Bed Bureau' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default MainStackNavigator
